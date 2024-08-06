f253dd055cc0e22430f298dc3794a46f30c7b7bd
Denys <denys1984.25@gmail.com>
2024-08-05 20:35:57 -0600

    first commit

---
+++ components/shared/index.ts
@@ -0,0 +1,4 @@
+export { Container } from "./Container";
+export { Header } from "./Header";
+export { Title } from "./Title";
+export { Categories } from "./Categories";
---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./button";
+export { Skeleton } from "./skeleton";
+export { Select } from "./select";
+export { Slider } from "./slider";
+export { Checkbox } from "./checkbox";
+export { Dialog } from "./dialog";
+export { Drawer } from "./drawer";
+export { Input } from "./input";
+export { Popover } from "./popover";
