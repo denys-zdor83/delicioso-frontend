5d81ff4763761a03fb9feee8229d4ae10bdcf882
Denys <denys1984.25@gmail.com>
2024-08-16 19:50:50 -0600

    Search: Set search input

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { cn } from '@/lib/utils'
 
-import { Container } from './'
+import { Container, SearchInput } from './'
 import Image from 'next/image'
 import { Button } from '../ui'
 import { ArrowRight, ShoppingCart, User } from 'lucide-react'
+import Link from 'next/link'
 
 type Props = {
     className?: string
@@ -16,12 +17,17 @@ export const Header: React.FC<Props> = ({ className }) => {
       <Container className="flex items-center justify-between py-8">
 
         {/* Left part of the header */}
-        <div className="flex items-center gap-4">
+        <Link href="/" className="flex items-center gap-4">
             <Image src="/logo.png" alt="logo" width={35} height={35} />
             <div>
                 <h1 className="text-2xl uppercase font-black">Deliciouso</h1>
                 <p className="text-sm text-gray-400 leading-3">Order food online</p>
             </div>
+        </Link>
+
+        {/* Middle part of the header */}
+        <div className="mx-10 flex-1">
+          <SearchInput />
         </div>
 
         {/* Right part of the header */}
