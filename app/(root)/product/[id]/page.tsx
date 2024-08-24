4519a5cafadb49092f502c5fbd9e5fc54ba05b2a
Denys <denys1984.25@gmail.com>
2024-08-24 13:58:33 -0600

    Modal: Prepared different forms for choosing pizza and choosing another product

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { Container, GroupVariants, ProductImage, Title } from "@/components/shared";
+import { Container, GroupVariants, PizzaImage, Title } from "@/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
-    const product = await prisma.product.findUnique({
+    const product = await prisma.product.findFirst({
         where: {
             id: Number(id),
         }
@@ -15,7 +15,7 @@ export default async function ProductPage({ params: { id } }: { params: { id: st
     return (
         <Container className="flex flex-col my-10">
             <div className="flex flex-1">
-                <ProductImage imageUrl={product.imageUrl} size={40} />
+                <PizzaImage imageUrl={product.imageUrl} size={40} />
 
                 <div className="w-[400px] bg-[#f7f6f5] p-7">
                     <Title 
