d6ff35965867dde751bba1cc8a25ed51ad2d0642
Denys <denys1984.25@gmail.com>
2024-09-10 14:40:04 -0600

    Cart: Created deletion of a product from the cart

--- shared/store/cart.ts
+++ shared/store/cart.ts
@@ -22,7 +22,7 @@ export interface CartState {
 //   addCartItem: (values: CreateCartItemValues) => Promise<void>;
 
   /* Запрос на удаление товара из корзины */
-//   removeCartItem: (id: number) => Promise<void>;
+  removeCartItem: (id: number) => Promise<void>;
 }
 
 export const useCartStore = create<CartState>((set, get) => ({
@@ -57,25 +57,25 @@ export const useCartStore = create<CartState>((set, get) => ({
     }
   },
 
-//   removeCartItem: async (id: number) => {
-//     try {
-//       set((state) => ({
-//         loading: true,
-//         error: false,
-//         items: state.items.map((item) => (item.id === id ? { ...item, disabled: true } : item)),
-//       }));
-//       const data = await Api.cart.removeCartItem(id);
-//       set(getCartDetails(data));
-//     } catch (error) {
-//       console.error(error);
-//       set({ error: true });
-//     } finally {
-//       set((state) => ({
-//         loading: false,
-//         items: state.items.map((item) => ({ ...item, disabled: false })),
-//       }));
-//     }
-//   },
+  removeCartItem: async (id: number) => {
+    try {
+      set((state) => ({
+        loading: true,
+        error: false,
+        // items: state.items.map((item) => (item.id === id ? { ...item, disabled: true } : item)),
+      }));
+      const data = await Api.cart.removeCartItem(id);
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true });
+    } finally {
+      set((state) => ({
+        loading: false,
+        // items: state.items.map((item) => ({ ...item, disabled: false })),
+      }));
+    }
+  },
 
 //   addCartItem: async (values: CreateCartItemValues) => {
 //     try {
