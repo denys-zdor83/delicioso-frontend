91bd33725d30c052b71ab34ef0fcbc83658a6041
Denys <denys1984.25@gmail.com>
2024-09-22 14:46:23 -0600

    Order-page: Created in order-page Cart block and Total Price block

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -1,6 +1,5 @@
 export { calcTotalPizzaPrice } from "./calc-total-pizza-price";
 export { getAvailablePizzaSizes } from "./get-available-pizza-sizes";
-export { usePizzaOptions } from "./use-pizza-options";
 export { getPizzaDetails } from "./get-pizza-details";
 export { getCartItemDetails } from "./get-cart-item-details";
 export { getCartDetails } from "./get-cart-details";
