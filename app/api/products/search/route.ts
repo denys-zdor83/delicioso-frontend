354ce9fd2f7f10222622f097ce13fa28a1422f10
Denys <denys1984.25@gmail.com>
2024-11-04 16:34:47 -0700

    Order-page: Main functionality for PayPal payment is ready

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -2,17 +2,17 @@ import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-    const query = req.nextUrl.searchParams.get('query') || '';
+  const query = req.nextUrl.searchParams.get('query') || '';
 
-    const products = await prisma.product.findMany({
-        where: {
-          name: {
-            contains: query,
-            mode: 'insensitive',    
-          },
-        },
-        take: 5,
-      });
-    
-      return NextResponse.json(products);
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: query,
+        mode: 'insensitive',    
+      },
+    },
+    take: 5,
+  });
+  
+  return NextResponse.json(products);
 }
