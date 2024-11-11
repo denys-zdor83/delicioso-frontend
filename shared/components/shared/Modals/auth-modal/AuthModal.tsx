7a86ae5de53bc12d9a46f0f34418d729394a06b7
Denys <denys1984.25@gmail.com>
2024-11-10 21:54:43 -0700

    Next-auth: Created register form markup

--- shared/components/shared/Modals/auth-modal/AuthModal.tsx
+++ shared/components/shared/Modals/auth-modal/AuthModal.tsx
@@ -5,7 +5,7 @@ import { Dialog, DialogContent } from '@/shared/components/ui/dialog';
 import { signIn } from 'next-auth/react';
 import React from 'react';
 import { LoginForm } from './forms/login-form';
-// import { RegisterForm } from './forms/register-form';
+import { RegisterForm } from './forms/register-form';
 
 interface Props {
   open: boolean;
@@ -29,8 +29,7 @@ export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
         {type === 'login' ? (
           <LoginForm onClose={handleClose} />
         ) : (
-          // <RegisterForm onClose={handleClose} />
-          'Register'
+          <RegisterForm onClose={handleClose} />
         )}
 
         Form
