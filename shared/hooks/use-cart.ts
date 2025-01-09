f95f2477ecd7321c8b38568722090c01b3108018
Denys <denys1984.25@gmail.com>
2025-01-09 09:44:41 -0700

    Refactoring: Got rid of repeated function resposible for increasing and decreasing the number of chosen product. Created it in one place and imported where it is needed

--- shared/hooks/use-cart.ts
+++ shared/hooks/use-cart.ts
@@ -10,6 +10,7 @@ type ReturnProps = {
   updateItemQuantity: (id: number, quantity: number) => void;
   removeCartItem: (id: number) => void;
   addCartItem: (values: CreateCartItemValues) => void;
+  clickCountButton: (id: number, quantity: number, type: 'plus' | 'minus') => void;
 };
 
 export const useCart = (): ReturnProps => {
