a096ed4502f3e0c36f1d472afaf7846793e80036
Denys <denys1984.25@gmail.com>
2025-02-01 19:13:11 -0700

    Main: Added restrictions to order fields on the order page

--- shared/constants/checkout-form-schema.ts
+++ shared/constants/checkout-form-schema.ts
@@ -1,12 +1,18 @@
 import { z } from 'zod';
 
 export const checkoutFormSchema = z.object({
-  firstName: z.string().min(2, { message: 'First name must be at least 2 characters' }),
-  lastName: z.string().min(2, { message: 'Last name must be at least 2 characters' }),
+  firstName: z.string()
+              .min(2, { message: 'First name must be at least 2 characters' })
+              .max(10, { message: 'First name must be at most 10 characters' }),
+  lastName: z.string()
+              .min(2, { message: 'Last name must be at least 2 characters' })
+              .max(10, { message: 'Last name must be at most 10 characters' }),
   email: z.string().email({ message: 'Enter a valid email' }),
   phone: z.string().min(10, { message: 'Enter a valid phone number' }),
-  address: z.string().min(10, { message: 'Enter a valid address' }),
-  comment: z.string()
+  address: z.string()
+            .min(10, { message: 'Enter a valid address' })
+            .max(50, { message: 'Address must be at most 50 characters' }),
+  comment: z.string().max(300, { message: 'Comment must be at most 300 characters' })
 });
 
 export type CheckoutFormValues = z.infer<typeof checkoutFormSchema>;
